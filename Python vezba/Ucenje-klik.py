80511b5dbe2216f529ab95e2e183325cfb0df284
Mifa43 <mifa43kotez@gmail.com>
2020-02-25 15:54:24 +0100

    Upotreba \n ima ulogu entera , ispravljanje greski u programu

--- Python vezba/Ucenje-klik.py
+++ Python vezba/Ucenje-klik.py
@@ -2,18 +2,19 @@
 import os   # pomocu import naredbe pozvali smo vec postujece module
 import subprocess
 import webbrowser
-print ("Zdravo, srecno ucenje")
+print ("Zdravo, srecno ucenje !")
 
 solucija = int(input('''Odaberi sledece opcije  
 1.Otvori Notpad
 2.Otovri Google
 3.Otvori Youtube
-4.Opis Programa
+4.Otvori opis Programa
 '''))   # sa int input oznacili smo da je dozvoljeno samo unositi brojeve
 
 if solucija == 1:
     print ('Otvaram notpad')
-    os.system('Notepad.exe')    # otvara notpad.exe ili aplikacije u sistemu 
+    os.system('Notepad.exe')    # otvara notpad.exe ili aplikacije u sistemu
+    
 
 elif solucija == 2:
     print ('Otvaram Google')
@@ -24,16 +25,16 @@ elif solucija == 3:
     webbrowser.open ('https://www.youtube.com/watch?v=sV2t3tW_JTQ') # otvara stranice browsera 
 
 elif solucija == 4:
-    print ('Zdravo ja sam Python treba ti opis programa ?')
-    print("Da / Ne")    # koristi veliko slovo kako bi radilo
-    pomoc = input("")
+    print ('\nZdravo ja sam Python treba ti opis programa ?')
+    print("\nDa / Ne")    # koristi veliko slovo kako bi radilo
+    pomoc = input("\n")
     if pomoc == "Da":
         print ('Ja sam program koji sluzi za lakse ucenje')
-        print ('Ono sto ja radim jeste otvaranje stvari koje ti trebaju za ucenje')
+        print ('Ono sto ja radim jeste otvaranje stvari koje ti trebaju za ucenje:')
         print ('******************************************************************')
-        print ('Mogu da otvorim Notpad ako odaberes opciju 1')
-        print ('Mogu da otvorim Google ako odaberes opciju 2')
-        print ('Mogu da otvorim Youtube ako odaberes opciju 3')
+        print ('Mogu da otvorim Notpad ako odaberes opciju 1.')
+        print ('Mogu da otvorim Google ako odaberes opciju 2.')
+        print ('Mogu da otvorim Youtube ako odaberes opciju 3.')
         print ('******************************************************************')
         print ('Verzija-0.1')
     else:
