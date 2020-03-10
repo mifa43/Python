e6a165dfaa844ea30536add90eefb0c1f54187da
Mifa43 <mifa43kotez@gmail.com>
2020-03-10 20:46:08 +0100

    Dodat je komentar u progrmu a za destinacije upotrebite koje vama odgovaraju najvise

--- Shutil-kopiranje-pomeranje-fajlova/shutil-copy.py
+++ Shutil-kopiranje-pomeranje-fajlova/shutil-copy.py
@@ -4,4 +4,4 @@ src = "C:/src"  # u src je naapisana putanja faja iz kog pravimo kopiju
 dst = "C:/dst/sve"  # ovo je folder gde zelimo da pastujemo /sve pravi novi folder 
 # ako ne stavimo /sve izbacice gresku 
 
-shutil.copytree(src=src, dst=dst)
+shutil.copytree(src=src, dst=dst)   #ovim izvrsavamo proces
