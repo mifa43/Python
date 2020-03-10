8442291b8e5f46c75fab8d2a36fe0d6780b700c1
Mifa43 <mifa43kotez@gmail.com>
2020-03-10 21:27:10 +0100

    Dodati komentari u programu

--- Modul time funkcija/Time_modul.py
+++ Modul time funkcija/Time_modul.py
@@ -1,8 +1,8 @@
-import time
+import time # uvezli smo modul 
 
 from datetime import datetime
 
-sad = datetime.now()
+sad = datetime.now()    #varijable smo upoptrebili kako bi lakse pozvali program
 
-balkan = sad.strftime("%H:%M:%S")
+balkan = sad.strftime("%H:%M:%S:%Y")   # sati minuti sekunde ako dadte Y= years godina
 print("Vreme na Balkanu je: ", balkan)
