e3a43293f004bd0a5ab94169b810c763f41af9de
Mifa43 <mifa43kotez@gmail.com>
2020-03-11 19:03:55 +0100

    Dodao sam novu komandu za kreiranje u direktorijumu kome mi zelimo

--- Zip modul/kreiranje_zip.py
+++ Zip modul/kreiranje_zip.py
@@ -4,3 +4,6 @@ import zipfile  # ovo je modul pomocu kojega moze da se pravi zip arhiva
 pravizip = zipfile.ZipFile("test.zip", "w") #moze da se koristi i samo ova komanda za kreiranje zip arhive
 
 pravizip.write(physicalPathOfFile, logicalPathOfFileInZip)
+
+moj_zip = zipfile.ZipFile("D:/Back-up/py/zip.zip", mode = 'w', compression=zipfile.ZIP_DEFLATED) 
+# ^---> Ovako se navodi direktorijum u kome zelim da napravim zip fajl 
