c5acb430c109439628067432ba715a91e50246a8
Mifa43 <mifa43kotez@gmail.com>
2020-03-11 18:09:09 +0100

    dodata napomena

--- Zip modul/kreiranje_zip.py
+++ Zip modul/kreiranje_zip.py
@@ -1,5 +1,6 @@
 import zipfile  # ovo je modul pomocu kojega moze da se pravi zip arhiva 
 #nemam dovoljno znanja ali ukombnovacu shutill i zipfile module kako bi 
 #odredio gde da kreira zip arhivu 
-pravizip = zipfile.ZipFile("test.zip", "w") 
+pravizip = zipfile.ZipFile("test.zip", "w") #moze da se koristi i samo ova komanda za kreiranje zip arhive
+
 pravizip.write(physicalPathOfFile, logicalPathOfFileInZip)
