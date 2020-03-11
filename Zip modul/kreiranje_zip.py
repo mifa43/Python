4a67f7b8aa559170a0946bad2cff4f97be51c6e5
Mifa43 <mifa43kotez@gmail.com>
2020-03-11 21:20:04 +0100

    Ispravljena greska dodat komentar

--- Zip modul/kreiranje_zip.py
+++ Zip modul/kreiranje_zip.py
@@ -1,11 +1,11 @@
 import zipfile  # ovo je modul pomocu kojega moze da se pravi zip arhiva 
- 
+
 pravizip = zipfile.ZipFile("test.zip", "w") #moze da se koristi i samo ova komanda za kreiranje zip arhive
 
 pravizip.write(physicalPathOfFile, logicalPathOfFileInZip)
 
-moj_zip = zipfile.ZipFile("D:/Back-up/py/zip.zip", mode = 'w', compression=zipfile.ZIP_DEFLATED)
-moj_zip.write('popara.txt') # ovako moze da se doda neki novi fajl u kreirani zip fajl 
+moj_zip = zipfile.ZipFile("D:/Back-up/py/zip.zip", mode = 'w', compression=zipfile.ZIP_DEFLATED) #za putanje
+moj_zip.writestr("poz.txt", "cao ti tamo") # ovako moze da se doda neki novi fajl u kreirani zip fajl 
 # ^---> Ovako se navodi direktorijum u kome zelim da napravim zip fajl 
 moj_zip.close() # koristi se za zatvaranje pre nego što izađete iz programa ili 
 #osnovni zapisi neće biti napisan
