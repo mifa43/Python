794223131a0dd442c3cd46c9326fd3bea5882068
Mifa43 <mifa43kotez@gmail.com>
2020-03-15 17:37:31 +0100

    Ovo je konacno sklopljen program koji ima ulogu Back-upa kopira fajlove sa diska C na disk D kreira folder i zip arhivu i stavlja sve sa C u zip u komentarima je opsirnije napisano doradicu ga jos

---
+++ Back-up/back-up.py
@@ -0,0 +1,46 @@
+# skalapanje
+import os 
+from datetime import datetime   # ovo znaci iz modula datetime importuj datetime jel u jednom modulu ima vise submodula koje posebno 
+# mozemo da izvlacimo i koristimo u nasem programu 
+import zipfile 
+
+# vreme kada smo importovali vreme upotrebili smo varijablu da zapisemo trenutno vreme nakon toga
+# koristimo varijablu vreme kako bi joj dodali sta da prikaze tj. sate minute i sekunde 
+trenutno = datetime.now()
+vreme = trenutno.strftime("%H_%M_%S")
+
+# u def smo definisali sta zelimo da prebacimo tj. zelimo sve da prebacimo
+def sve(direktorijum):
+
+    lista = [] # ovo je lista u kojoj upisujemo fajlove u direktorijumu, subdirektorijumu i root 
+   
+    for root, dirs, files in os.walk(direktorijum): # root , dirs , fails je sistemski upotrebljeno kako
+        # bi mogli da izvucemo fajlove i to tako mora da se pise kako bi os prepozno sta zelimo od njega
+        for f_ime in files: # f_ime mozemo da nazovemo kako god zelimo 
+            pch = os.path.join (root, f_ime) # ovde smo rekli os.path.join spoji/udruzi root-folder sa f_ime
+            lista.append(pch) # a ovde smo u listu zapisali te stavri koje se nalaze u root folderu 
+
+        return(lista)   # return vraca listu zabelezenih stringova
+
+src = "C:/src"  # src je odakle se kopiraju fajlovi
+lista = sve(src)    # ovde smo rekli to sto zelimo da prebaci da se nalazi src 
+print(lista)  #< ovo je samo da vididte kako to izgleda 
+
+print('Konvertujem sa', src)  
+
+for fajl in lista:  # ovde bukv kaze za fajl u listi prikazi liste i to radi 
+    print(lista)
+
+os.mkdir("D:/Back-up")  # ova komanda kreira folder na D lokiaciji 
+
+with zipfile.ZipFile("D:/Back-up/zip.zip", 'w') as moj_zip: # ovde kreiramo zip fajl 
+    moj_zip.writestr ("Proces_je_izvrsen.txt","Vreme izvrsavanja Back-up/a je: {0}".format(vreme))  # ovo je napisano izdvojeno kako ne bi doslo do greske ili vise puta ubacio jedan te isti fajl 
+    
+    # ^--- mojzip writestr pravi txt fajl i upisuje stringove i vreme koje smo definisali gore 
+    for fajl in lista:  # ovde kopira se sa c i prebacuje u d za fajl u listi prepisi/kopiraj/zapisi u moj_zip a to je nasa zip arhiva koju smo kreirali 
+        moj_zip.write (fajl)
+        
+ 
+print("Back-up je uspesno izvrsen, ukupno prebaceno fajlova: {0}".format(len(lista))) # ovo je potvrda 
+# da je sve izvrseno i len nam racuna koliko fajlova imamo prebacenih 
+        
