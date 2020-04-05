095972a76bfa0e7edd1d7369489772bc5ecb1505
Mifa43 <mifa43kotez@gmail.com>
2020-04-05 11:31:38 +0200

    Dodati komentari

--- With komanda/With.py
+++ With komanda/With.py
@@ -1,6 +1,8 @@
 
 # ovo je slicno kao prethodni primer sa try i finally samo sto ovde koristimo with komandu
 # with komanda nam otvara fajl i po kraju programa on automacki zatvara dokument
+# to je zato sto with komanda ima standarde i kada je pokrecemo ona u pozadini
+# radi __enter__ i __exit__ otvara i zatvara bez close()
 with open('pesma.txt') as f:
     for linija in f:
         print(linija, end = ' ')
