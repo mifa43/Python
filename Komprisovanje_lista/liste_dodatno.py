31c7477e1a8748751ae9dda4756167162a65f821
Mifa43 <mifa43kotez@gmail.com>
2020-04-12 13:07:52 +0200

    Primer komprinovanja lista

---
+++ Komprisovanje_lista/liste_dodatno.py
@@ -0,0 +1,9 @@
+prva_lista = [2, 3, 4]
+druga_lista = [2 * i for i in prva_lista if i > 2]
+print(druga_lista)
+
+h_lista = []    
+
+for lista in 'Beograd': 
+    h_lista.append(lista)
+print(h_lista)
