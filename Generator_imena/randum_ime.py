87a30625052aaadbbf90d1ffdac32be9c50a5dbe
Mifa43 <mifa43kotez@gmail.com>
2020-04-29 23:20:53 +0200

    Nasumican generator imena OOP

---
+++ Generator_imena/randum_ime.py
@@ -0,0 +1,22 @@
+import random  # modul za randum izbor
+
+lista_imena = ("Minjole", "drvar")     # lista zeljenih reci
+lista_imena_sporedna = ("topologicar", "sekac pica")  # druga lista
+
+class Imena:   # klasa 
+   def __init__(self, ime_jedan, ime_dva, konacno):   # privatna klase sa promenljivama
+      self.ime_jedan = ime_jedan
+      self.ime_dva = ime_dva
+      self.konacno = konacno  # definisanje promenljivih pomocu kljucne reci self
+
+   def generator(self):
+      global lista_imena
+      global lista_imena_sporedna
+
+      self.ime_jedan = random.choice(lista_imena)     # bira nasumicno ime iz liste ime
+      self.ime_dva = random.choice(lista_imena_sporedna)
+      self.konacno = (self.ime_jedan + " " + self.ime_dva)  # spaja dva imena i odvaja spejsom
+      print("Ime je: {0}".format(self.konacno)) # prikazujemo rezultat na ekranu
+
+l = Imena("prvi","drugi","treci")
+l.generator()  # pozivanje 
