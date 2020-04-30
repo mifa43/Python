8ed9394e53bffb5fdeeaa79cdebc81d401d94463
Mifa43 <mifa43kotez@gmail.com>
2020-04-30 11:27:31 +0200

    izmenjena komanda za nasumican izbor imena iz lista_ime.. neophodno odkomentarisati random.choice na liniji 16

--- Generator_imena/randum_ime.py
+++ Generator_imena/randum_ime.py
@@ -1,7 +1,7 @@
 import random  # modul za randum izbor
 
-lista_imena = ("Minjole", "drvar")     # lista zeljenih reci
-lista_imena_sporedna = ("topologicar", "sekac pica")  # druga lista
+lista_imena = ("Minjole")     # lista zeljenih reci
+lista_imena_sporedna = ("topologicar", "sekac pica", "Nigga old franja papa")  # druga lista
 
 class Imena:   # klasa 
    def __init__(self, ime_jedan, ime_dva, konacno):   # privatna klase sa promenljivama
@@ -13,7 +13,7 @@ class Imena:   # klasa
       global lista_imena
       global lista_imena_sporedna
 
-      self.ime_jedan = random.choice(lista_imena)     # bira nasumicno ime iz liste ime
+      self.ime_jedan = lista_imena #random.choice(lista_imena)     # bira nasumicno ime iz liste ime
       self.ime_dva = random.choice(lista_imena_sporedna)
       self.konacno = (self.ime_jedan + " " + self.ime_dva)  # spaja dva imena i odvaja spejsom
       print("Ime je: {0}".format(self.konacno)) # prikazujemo rezultat na ekranu
