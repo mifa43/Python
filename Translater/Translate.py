823a89da9dd829763550871058483bdae350fd7f
Mifa43 <mifa43kotez@gmail.com>
2020-05-12 10:51:59 +0200

    Ovo je python prevodilac za ovo je poptrebno da se instalira modul googletrans

--- Translater/Translate.py
+++ Translater/Translate.py
@@ -1,1 +1,10 @@
-from googletrans import Translator
+from googletrans import Translator  # ovo je modul google translate koji nudi python 
+
+prevodilac = Translator()
+
+unos_tekst = input("Unesi tekst za prevod: ")   # ovo je unos za tekst koji se prevodi
+prevod = prevodilac.translate(unos_tekst, src="sr", dest="en")  # src = sr sa srpskog na engleski 
+# za vise jezika moramo da ukucamo print(LANGUAGES)
+
+
+print(unos_tekst,"--->",prevod.text)    #prevod.text nam pokazuje tekts koji se preveo
