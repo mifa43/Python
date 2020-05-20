7dfcde1e255e92f0626d6488316a4dfd27aa7717
Mifa43 <mifa43kotez@gmail.com>
2020-05-20 11:20:21 +0200

    Greska u programu

--- total/total.py
+++ total/total.py
@@ -1,12 +1,15 @@
     # total 
 def total(inicijal=10, *brojevi, **kljucnereci):"""pojavljuje se greska jel sam 
-                                                        vise puta definisu istu stvar kopiraj odvojeno svaki prmier"""
-    brojanje = inicijal
-    for broj in brojevi:  
-        brojanje += broj   
-    for rec in kljucnereci: 
-        brojanje +=kljucnereci[rec]    
-    return brojanje
+                                                        vise puta definisao istu stvar kopiraj odvojeno svaki prmier"""
+    # brojanje = inicijal
+
+    # # for broj in brojevi:  
+    # #     brojanje += broj   
+
+    # for rec in kljucnereci: 
+    #     brojanje +=kljucnereci[rec]    
+    #     return brojanje
+                                        #postoji greska
 print(total(10, 1, 2, 3, povrce = 50, voce = 100)) 
 
 #ponovi 41 total nakon sto naucis sta je tuple i druge termine 
