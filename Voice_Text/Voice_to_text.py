b4bfa13a85d8e04daa086c4896ca812f26fdd14d
Mifa43 <mifa43kotez@gmail.com>
2020-09-16 12:51:59 +0200

    Dodat je uslov za izvrsavanje pomocu njega mozemo da otvorimo chrome uskoro cu i da dodam otvaranje nekih sajtova kao npr youtube.. i druge

--- Voice_Text/Voice_to_text.py
+++ Voice_Text/Voice_to_text.py
@@ -3,10 +3,14 @@ from scipy.io.wavfile import write
 import winsound
 import speech_recognition as sr
 import soundfile as sf
+import webbrowser
+import subprocess
 
+lista = []
 unos = int(input("Unesi broj '1' za unos glasovne poruke:"))
 
 def rekorduj():
+    global lista
     fs = 44100  # frekvencija u hz
     seconds = 5  # trajanje zvuka
     mojrekording = sd.rec(int(seconds * fs), samplerate=fs, channels=2) # kanali za snimanje
@@ -26,7 +30,28 @@ def rekorduj():
     hard = sr.AudioFile('output.flac') # audio fajl koji citamo 
     with hard as source:    # otvaranje i konvertovanje audio fajla u tekst 
         audio = r.record(source)
-    print(r.recognize_google(audio))
+    rec_1 = r.recognize_google(audio)
+
+    lista.insert(0, rec_1)  # u listu se dodaje izgovorena rec
+    print(lista[0])
+    for i in lista:     # sa for petljom proveravamo reci koje su unesene
+        lista_cmds = ['open', 'do']     # lista cmd je lista kljucnih reci koje mozemo da pretvorimo u komande za izvrsavanje 
+        for n in lista_cmds:    # proveravamo dali u listi izgovorene reci postoji i rec koja opisuje neku od reci iz liste komande
+            if 'open' in i and 'open' in n:     # uslov ako je izgovorena rec open i ta rec ima u listi komanda izvrsavamo otvaranje necega >!< za sada je to otvaranje googla 
+               
+               
+                subprocess.call("C:\Program Files (x86)\Google\Chrome\Application\chrome.exe")
+                webbrowser.open("https://www.youtube.com")
+                print("+1")
+
+            
+            
+
+   
+
+
+
+
 
 if unos == 1:
     rekorduj()
