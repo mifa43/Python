21d1878963a372ace264747b992e10f1d04ebc6a
Mifa43 <mifa43kotez@gmail.com>
2020-09-16 10:33:56 +0200

    Ovo je moj novi projekat na kome radim nazivam ga 'simple voice assistant'.. Ima cilj da na osnovu izgovorene reci izvrsi neku radnju za pocetask ce da bude otvaranje web browsera naknado cu da pokusam da instalira python module i jos neke korisne stvari

--- Voice_Text/Voice_to_text.py
+++ Voice_Text/Voice_to_text.py
@@ -1,10 +1,32 @@
+import sounddevice as sd
+from scipy.io.wavfile import write
+import winsound
+import speech_recognition as sr
+import soundfile as sf
 
+unos = int(input("Unesi broj '1' za unos glasovne poruke:"))
 
-import speech_recognition as sr 
-with sr.Microphone() as source:
-    # read the audio data from the default microphone
-    audio_data = r.record(source, duration=5)
-    print("Recognizing...")
-    # convert speech to text
-    text = r.recognize_google(audio_data)
-    print(text)
+def rekorduj():
+    fs = 44100  # frekvencija u hz
+    seconds = 5  # trajanje zvuka
+    mojrekording = sd.rec(int(seconds * fs), samplerate=fs, channels=2) # kanali za snimanje
+    sd.wait()  # pauziranje kako bi se snimijo zvuk
+    write('output.wav', fs, mojrekording)  # pisanje i cuvanje zvuka u wav format
+
+    winsound.Beep(1000, 100)    # beep winsound modul za slusanje snimljenog zvuka
+    filename = "output.wav" # ekstenzija fajla je wav 
+    winsound.PlaySound(filename, winsound.SND_FILENAME) #  plejer 
+
+    data, samplerate = sf.read('output.wav')    # modul za konvertovanje wav fajla u \
+    sf.write('output.flac', data, samplerate)  # flac fajl 
+    # nisam bas strucan za audio produkciju ali koliko sam procitao razlika izmedju wav i flaca je u frekvenciji i kanala emitovanja
+    # i iz tog razloga speech_recognition modul za pisanje zvucnih fajlova u txt fajlove ne moze da koristi wav, mp3.. itd audio formate ali mu odgovara flac
+
+    r = sr.Recognizer() 
+    hard = sr.AudioFile('output.flac') # audio fajl koji citamo 
+    with hard as source:    # otvaranje i konvertovanje audio fajla u tekst 
+        audio = r.record(source)
+    print(r.recognize_google(audio))
+
+if unos == 1:
+    rekorduj()
