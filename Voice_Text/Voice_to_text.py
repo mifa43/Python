26cbd062afc4a6eff1d0cadf8bea450e53329a2b
Mifa43 <mifa43kotez@gmail.com>
2020-09-17 15:37:28 +0200

    Izmene u programu: promenjen je uslov ulaska u if napocetku je bilo 'open' i 'google' posto vec postoji if za open ako nema open komande onda nece uci u if za otvaranje linka tako da je bitno da se otvori bilo sta potrebna je komanda open pa kljuca rec google,youtube ili python za pydocs redosled izgovorenih reci nije toliko vazan jel se pravi provera da lli komanda postoji u listi cmds ako poastoji i izgovorena je komanda open pravi se provera za kljuce reci u listi za open ako rec koja je izgovorena postoji u toj listi izvrsava se neki od if blokova npr. "google open ili open google" daje isti rezultat

--- Voice_Text/Voice_to_text.py
+++ Voice_Text/Voice_to_text.py
@@ -33,24 +33,37 @@ def rekorduj():
     rec_1 = r.recognize_google(audio)   # izgovorenu recenicu stavljamo u variablu
     rec_2 = rec_1.split(" ")    # zatim pomocu splita rastavljamo recenicu i stavljamo u listu
     lista_cmds = ['open', 'do']     # lista komandi koje mogu da se izvrsavaju npr open ima svoju listu sta moze da otvara dok do nema nista za sad
-    lista_za_open = ['YouTube', 'Google', 'docs.python.org']    # lista cmds ima komandu open komanda open ima listu sadrzaja sta moze da otvori
-
+    lista_za_open = ['YouTube', 'Google', 'Python']    # lista cmds ima komandu open komanda open ima listu sadrzaja sta moze da otvori
+    
     provera = any(item in rec_2 for item in lista_cmds)     # any funkcija pravi proveru da li rec_2 i lista_cmds ima zajednicko nesto ako ima vraca nam bool true ili false ako nema
     if provera == True:     # ako je tacno ulazimo u sledeci blok
         for i in rec_2:     # proveravamo reci iz liste i liste cmds ako postoje pomocu filtera mozemo da je ispisemo 
             filt_object = filter(lambda a : i in a, lista_cmds)
             provera_2 = any(item in rec_2 for item in lista_za_open)
             if provera_2 == True:
-                    filt_object_2 = filter(lambda b : i in b, lista_za_open)
-                    if ('open' and 'Google' in list(filt_object), list(filt_object_2)): #filte_object pretvaramo u listu kako bi mogli da utvrdimo da li su izgovorene reci odgovarajuce ovom bloku
-                        subprocess.call("C:\Program Files (x86)\Google\Chrome\Application\chrome.exe") # subprocess otvara chrome ako filter objekti sadrze dve navedene reci open i google
+                    filt_object_2 = (filter(lambda b : i in b, lista_za_open))
+                    konvert = ''.join(list(filt_object))#filte_object pretvaramo u listu kako bi mogli da utvrdimo da li su izgovorene reci odgovarajuce ovom bloku
+                    konvert_2 = ''.join(list(filt_object_2))    # flit_ogject je konvertovan u listu ali sa join pretvaramo u string tj. rec koja je izgovorena a pritom je komanda iz list_cmds
+                   
+                    if (konvert == 'Google' or konvert_2 == 'Google'): 
+                        subprocess.call("C:\Program Files (x86)\Google\Chrome\Application\chrome.exe") # subprocess otvara chrome ako filter objekti sadrze navedenu recu if uslovu google
                         break   # nakon otvaranja taba brejkujemo program
+                    
+                    if (konvert == 'YouTube' or konvert_2 == 'YouTube'):    #uslovi za otvaranje su promenjeni vise nema open iz razloga jel taj uslov vec postoji da nismo rekli open ne bi ni dosli do ovog bloka
+                        url = 'http://youtube.com/'     # url stranice koju otvaramo
+                        chrome_path = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s'  # ovo je lokacija chroma
+                        webbrowser.get(chrome_path).open(url, new=1)    # prosledjujemo path i otvaramo url u novom prozoru
+                        break   # nakon izvrsavanja brejkujemo program
+
+                    if (konvert == 'Python' or konvert_2 == 'Python'):  
+                        url = 'http://docs.python.org/'     
+                        chrome_path = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s'  
+                        webbrowser.get(chrome_path).open(url, new=1)   
+                        break   
             else:
-                print("ova komanda to ne moze")
+                print("Ova komanda to ne moze da izvrsi taj proces")
     else:
         print("Rec nije komanda")
 
-        #webbrowser.open("https://www.youtube.com")
-            
 if unos == 1:
     rekorduj()
