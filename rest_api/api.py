6f30a41e1b7d1814860376f211a562d69c3b31cd
Mifa43 <mifa43kotez@gmail.com>
2021-01-18 17:29:58 +0100

    kreiranje restful api-a sa JWT autorizacijom uz postmana za sada smo uspeli da kreiramo bazu, da upisemo podatke pomocu postmana u json formatu, pretrazivanje svih korisnika pomocu postman requesta i get metode kao i filtriranje samo jednog korisnika sa public_id-om. podsetnik : sreiti podizanje greske o tome da nije pronadjen korisnik sa public_id-om  koji je unesen

---
+++ rest_api/api.py
@@ -0,0 +1,109 @@
+from flask import Flask, request, jsonify, session
+from flask_mysqldb import MySQL
+import os
+import uuid
+from werkzeug.security import generate_password_hash, check_password_hash 
+
+
+app = Flask(__name__)
+dbpass = os.getenv('dbpassword')
+app.config['SECRET_KEY'] = "ovojemojtajnikljuc"
+app.config['MYSQL_HOST'] = "localhost"
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = dbpass
+app.config['MYSQL_DB'] = 'rest_api'
+
+db = MySQL(app)
+app.app_context().push()
+# class User():
+#     cur = db.connect.cursor()
+#     query = "CREATE TABLE rest_api (id INT NOT NULL PRIMARY KEY,public_id INT, name  VARCHAR(40),password VARCHAR(30), is_admin BOOLEAN)"
+#     cur.execute(query)
+#     db.connect.commit()
+#     cur.close()
+
+# class Todo():
+#         cur = db.connect.cursor()
+#         query = "CREATE TABLE todo (id INT NOT NULL PRIMARY KEY,public_id INT, text  VARCHAR(40),complete BOOLEAN, user_id BOOLEAN)"
+#         cur.execute(query)
+#         db.connect.commit()
+#         cur.close()
+class Insert(): # Upisivanje u bazu pomocu konstruktora promenljivih vrednosti
+    def __init__(self, public_id, name, password, is_admin):    # metoda sa parametrima
+        self.public_id = public_id  # deklaracija varijabli
+        self.name = name
+        self.password = password
+        self.is_admin = is_admin
+
+        cur = db.connection.cursor()
+        query = "INSERT INTO rest_api.rest_api (public_id, name, password, is_admin) VALUES ('{0}','{1}','{2}',{3});".format(self.public_id, self.name, self.password, self.is_admin)
+        cur.execute(query)
+        db.connection.commit()
+        cur.close()
+
+
+@app.route('/user', methods = ['GET'])
+def get_all_users():
+    output = []
+    cur = db.connection.cursor()
+    query = "SELECT public_id, name, password, is_admin FROM rest_api.rest_api;"
+    cur.execute(query)
+    db.connection.commit()
+    cur.close()
+    for i in cur:
+        user_data = {}
+        user_data['public_id'] = i[0]
+        user_data['name'] = i[1]
+        user_data['password'] = i[2]
+        user_data['is_admin'] = i[3]
+        output.append(user_data)
+
+    
+    return jsonify({"Users": output})
+
+@app.route('/user/<public_id>', methods = ['GET'])
+def get_one_user(public_id):
+    
+    cur = db.connection.cursor()
+    query = "SELECT public_id, name, password, is_admin FROM rest_api.rest_api WHERE public_id='{0}';".format(public_id)
+    cur.execute(query)
+    db.connection.commit()
+    cur.close()
+    if not public_id:   #sledeci korak sredjivanje izuzetka 
+        return jsonify({"Message" : "User not found"})
+    for i in cur:
+        user_data = {}
+        user_data['public_id'] = i[0]
+        user_data['name'] = i[1]
+        user_data['password'] = i[2]
+        user_data['is_admin'] = i[3]
+        
+
+    
+    return jsonify({"Users": user_data})
+
+    
+@app.route('/user', methods = ['POST'])
+def create_user():
+    data = request.get_json()
+
+    hash_password = generate_password_hash(data['password'], method = 'sha256')
+
+    new_user = Insert(str(uuid.uuid4()), data['name'], hash_password, 0) #nula simbolizuje false a jedan true
+    return jsonify({"Message" : "New user created!"})
+
+
+@app.route('/user/<public_id>', methods = ['PUT'])
+def promote_user(public_id):
+    return ""
+
+@app.route('/user/<public_id>', methods = ['DELETE'])
+def delete_user(public_id):
+    return ""
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug = True)
